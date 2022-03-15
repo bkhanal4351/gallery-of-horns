5ad3588fce4bcf60869819d5eaeb9b28fefdb4a5
Bishal Khanal <bishal.khanal87@gmail.com>
2022-03-15 15:08:37 -0700

    add json file and render images

--- src/Hornedbeast.js
+++ src/Hornedbeast.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class Hornedbeast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      votes:0
+    }
+  }
 
   render(){
     return (
